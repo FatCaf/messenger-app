dd883e97f9447affa1fb4684d79f91271c6a2b07
Mykyta Neklesa <nikitaneklesa5@gmail.com>
2024-06-21 15:14:45 +0300

    add: enums

---
+++ back/src/enums/app-route.ts
@@ -0,0 +1,7 @@
+export enum AppRoute {
+	ROOT = '/api/v1',
+	USER = '/users',
+	USER_LOGIN = '/login',
+	USER_CREATE = '/register',
+	USER_GET_CONTACTS = '/contacts/:id',
+}
---
+++ back/src/enums/errors-messages.ts
@@ -0,0 +1,6 @@
+export enum ErrorMessages {
+	USER_ALREADY_EXISTS = 'User with this email already exists, use it to login into your account!',
+	USER_UNEXPECTED_ERROR = 'Unexpected error while creating user!',
+	USER_UNAUTHORIZED = 'Wrong login attempt, invalid login or password!',
+	JWT_UNDEFINED = 'JWT_SECRET is not defined or is empty',
+}
