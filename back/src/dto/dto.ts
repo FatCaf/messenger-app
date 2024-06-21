815bfe43149612791f95af423fdd9dc5366bd96c
Mykyta Neklesa <nikitaneklesa5@gmail.com>
2024-06-21 15:11:27 +0300

    add: user-dto-with-password, user-login dtos

--- back/src/dto/dto.ts
+++ back/src/dto/dto.ts
@@ -1,4 +1,13 @@
 import { UserCreateDto } from './user-create-dto';
 import { UserCreateSuccessDto } from './user-create-success-dto';
-import { UserDto } from './user-dto';
-export { UserCreateDto, UserCreateSuccessDto, UserDto };
+import { UserDto, UserDtoWithPassword } from './user-dto';
+import { UserLoginDto } from './user-login-dto';
+import { UserLoginSuccessDto } from './user-login-success-dto';
+export {
+	UserCreateDto,
+	UserCreateSuccessDto,
+	UserDto,
+	UserDtoWithPassword,
+	UserLoginDto,
+	UserLoginSuccessDto,
+};
