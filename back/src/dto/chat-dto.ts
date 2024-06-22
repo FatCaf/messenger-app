f1d5eebe676c118c0cecbac1382379770e7bab08
Mykyta Neklesa <nikitaneklesa5@gmail.com>
2024-06-22 08:25:30 +0300

    add: chat and message dtos

---
+++ back/src/dto/chat-dto.ts
@@ -0,0 +1,13 @@
+import { admin } from '../db/init-db';
+import { MessageDto } from './message-dto';
+
+export class ChatDto {
+	constructor(
+		readonly id: string,
+		readonly createdAt: admin.firestore.Timestamp,
+		readonly participants: [string, string],
+		readonly lastMessage?: string,
+		readonly lastMessageTimeStamp?: admin.firestore.Timestamp,
+		readonly messages?: MessageDto[]
+	) {}
+}
